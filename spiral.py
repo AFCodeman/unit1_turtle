77e9b8e74a2748946d78e7ee1cc743b96ef50d5f
anthonyfagiolo <fagioan@ssfs.org>
2021-09-09 11:16:40 -0400

    made a spiral

--- spiral.py
+++ spiral.py
@@ -1,3 +1,18 @@
 import turtle
 
-for x in range ()
+def draw_spiral(side_length):
+    for x in range(2):
+        turtle.forward(side_length)
+        turtle.right(90)
+
+draw_spiral(100)
+draw_spiral(110)
+draw_spiral(120)
+draw_spiral(130)
+draw_spiral(140)
+draw_spiral(150)
+draw_spiral(160)
+draw_spiral(170)
+draw_spiral(180)
+draw_spiral(190)
+draw_spiral(200)
